1e75021f89455fe549cf785f1c395e74ea89e248
dhkim03182 <kimdohyun20030318@gmail.com>
2026-08-28 16:44:30 +0900

    Update header.js
    오류수정

--- js/header.js
+++ js/header.js
@@ -7,45 +7,27 @@ document.addEventListener("DOMContentLoaded", function () {
         return;
     }
 
+    const headerPath =
+        new URL("../include/header.html", document.currentScript.src);
 
-    /*
-    =========================================
-    header.html 불러오기
-    =========================================
-
-    현재 페이지가 어느 폴더에 있든
-    신화비즈 사이트의 루트 기준으로
-    include/header.html을 찾도록 설정
-    */
-
-    fetch("/include/header.html")
-
+    fetch(headerPath)
         .then(response => {
 
             if (!response.ok) {
-
-                throw new Error(
-                    "header.html을 불러오지 못했습니다."
-                );
-
+                throw new Error("header.html을 불러오지 못했습니다.");
             }
 
             return response.text();
 
         })
-
         .then(data => {
 
             headerContainer.innerHTML = data;
 
         })
-
         .catch(error => {
 
-            console.error(
-                "헤더 로딩 오류:",
-                error
-            );
+            console.error(error);
 
         });
 
